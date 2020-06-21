3523eda232581274deedcac4a6c8b25297773575
Loarlam <messiah02@mail.ru>
2020-06-21 18:31:56 +0300

    Добавлены: возможность сохранять скриншоты внутренней области формы, с возможностью выбора папки и расширения (jpeg, png, bmp);  WebBrowser в правой части формы; третья панель, объединяющая в себе остальные панели формы.

--- FindByIp/Form1.cs
+++ FindByIp/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
 
 namespace FindByIp
 {
@@ -6,18 +10,22 @@ namespace FindByIp
     {
         bool visibleOrNot;
         int defaultWidthOfPanel;
+        SaveFileDialog saveFileDialog;
 
         public Form1()
         {
             InitializeComponent();
 
+            panelForMap.Hide();
             defaultWidthOfPanel = panelForInformation.Width;
             panelForInformation.Width += 400;
             visibleOrNot = false;
+            saveFileDialog = new SaveFileDialog { InitialDirectory = $@"C:\Users\{Environment.UserName}\Desktop\" };
+            saveFileDialog.FileName = "Screenshot";
         }
 
         /*Плавное появление формы*/
-        void TimerToSmoothlyRunForm_Tick(object sender, System.EventArgs e)
+        void TimerToSmoothlyRunForm_Tick(object sender, EventArgs e)
         {
             if (Opacity == 1)
                 timerToSmoothlyRunForm.Stop();
@@ -25,14 +33,15 @@ namespace FindByIp
                 Opacity += .04;
         }
 
-        void Button1_Click(object sender, System.EventArgs e)
+        void Button1_Click(object sender, EventArgs e)
         {
+            panelForMap.Show();
             timerForSlidingPanelInformation.Start();
         }
 
         /* По дефолту panelForInformation во всю форму, а значит panelForMap с картой не видна (visibleOrNot = false)
          * От сих и первое действие - уменьшение значения ширины на 10*/
-        void TimerForSlidingPanelInformation_Tick(object sender, System.EventArgs e)
+        void TimerForSlidingPanelInformation_Tick(object sender, EventArgs e)
         {
             if (!visibleOrNot)
             {
@@ -56,12 +65,30 @@ namespace FindByIp
             }
         }
 
+        void ScreenshotToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Title = "Сохранение скриншота";
+            saveFileDialog.Filter = "JPEG (*.jpg; *.jpeg; *.jpe) | *.jpg; *jpeg; *.jpe|PNG (*.png) | *.png|BMP (*.bmp) | *.bmp";
+            saveFileDialog.OverwritePrompt = true;
+            saveFileDialog.CheckPathExists = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap screenshot = new Bitmap(panelForScreenshot.Width, panelForScreenshot.Height);
+                panelForScreenshot.DrawToBitmap(screenshot, new Rectangle(0, 0, screenshot.Width, screenshot.Height));
+                saveFileDialog.InitialDirectory = Path.GetDirectoryName(saveFileDialog.FileName);
+                screenshot.Save(saveFileDialog.FileName);
+                saveFileDialog.FileName = Path.GetFileNameWithoutExtension(saveFileDialog.FileName);
+                Process.Start(saveFileDialog.InitialDirectory);
+            }
+        }
+
         void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             if (WindowState == FormWindowState.Minimized)
                 WindowState = FormWindowState.Normal;
         }
 
-        void CloseToolStripMenuItem_Click(object sender, System.EventArgs e) => Close();
+        void CloseToolStripMenuItem_Click(object sender, EventArgs e) => Close();
     }
 }
