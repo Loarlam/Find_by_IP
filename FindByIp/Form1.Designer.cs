c7ca77935cb644574c3aca7c32df1e65ed7b6609
Loarlam <messiah02@mail.ru>
2020-07-02 00:38:10 +0300

    Оно работает, но криво, но работает. Запутался, но разберусь.

--- FindByIp/Form1.Designer.cs
+++ FindByIp/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.screenshotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panelForScreenshot = new System.Windows.Forms.Panel();
             this.panelForInformation = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -111,6 +113,8 @@
             // 
             this.panelForInformation.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.panelForInformation.ContextMenuStrip = this.contextMenuStrip2;
+            this.panelForInformation.Controls.Add(this.button2);
+            this.panelForInformation.Controls.Add(this.label1);
             this.panelForInformation.Controls.Add(this.linkLabel1);
             this.panelForInformation.Controls.Add(this.textBox1);
             this.panelForInformation.Controls.Add(this.button1);
@@ -121,6 +125,34 @@
             this.panelForInformation.Name = "panelForInformation";
             this.panelForInformation.Size = new System.Drawing.Size(400, 561);
             this.panelForInformation.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(89, 280);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Сетевые подключения";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(60, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Отсутствует подключение к интернету!\r\n";
+            this.label1.Visible = false;
             // 
             // linkLabel1
             // 
@@ -180,12 +212,11 @@
             this.maskedTextBox1.BeepOnError = true;
             this.maskedTextBox1.Location = new System.Drawing.Point(142, 60);
             this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(0);
-            this.maskedTextBox1.Mask = "990\\.990\\.990\\.990";
+            this.maskedTextBox1.Mask = "###\\.###\\.###\\.###";
             this.maskedTextBox1.Name = "maskedTextBox1";
             this.maskedTextBox1.Size = new System.Drawing.Size(116, 24);
             this.maskedTextBox1.TabIndex = 0;
             this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.maskedTextBox1.ValidatingType = typeof(int);
             this.maskedTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MaskedTextBox1_KeyDown);
             // 
             // webBrowser1
@@ -245,6 +276,8 @@
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FindByIp/Form1.cs
+++ FindByIp/Form1.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.Win32;
+﻿/*
+Планы:
+1. Добавить перерисовку карты, если пользователь передвинул карту в сторону, только после того, как нажмет на кнопку "Раскрыть карту";
+2. Добавить: если интернет есть, и fileNameToSave не равно нулю, то сохраняем с названием: Город + Широта + Долгота. Иначе, дефолтное название;
+3. Продумать возможность избавления от webBrowser, и замену его на скриншот области с карты, с выводом оного в отдельную панель$
+4. Подумать над counterForFileName$
+5. Блокровать возможность сохранения файла, если в maskTextBox не изменился ip-адрес. Цвет текста в ContextMenuStrip заменить на красный, и сам текст поменять на "повторное сохранение невозможно";
+6. Починить ошибку, которая позволяла юзеру вводить больше ограниченного числа символов в maskTextBox.
+*/
+
+using Microsoft.Win32;
 using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,43 +24,46 @@ namespace FindByIp
 {
     public partial class Form1 : Form
     {
-        bool visibleOrNot;
-        int defaultWidthOfPanel, counter;
-        string maskTextBoxValue, pathToRegistryFolder = @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+        bool visibleOrNot, counterTrueOrFalse;
+        byte digitsInTheFileName;
+        short defaultWidthOfPanel;
+        string[] filesInFolder;
+        string maskTextBoxValue;
+        const string PATH_TO_REGISTRY_FOLDER = @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+        IPStatus status;
         SaveFileDialog saveFileDialog;
 
         public Form1()
         {
-            Registry.CurrentUser.OpenSubKey(pathToRegistryFolder, true).SetValue("FindByIp.exe", "69632", RegistryValueKind.DWord);
+            Registry.CurrentUser.OpenSubKey(PATH_TO_REGISTRY_FOLDER, true).SetValue("FindByIp.exe", "69632", RegistryValueKind.DWord);
 
             InitializeComponent();
 
-            counter = 0;
-            defaultWidthOfPanel = panelForInformation.Width;
+            counterTrueOrFalse = false;
+            defaultWidthOfPanel = Convert.ToInt16(panelForInformation.Width);
             panelForInformation.Width += 400;
             webBrowser1.Width -= 400;
             visibleOrNot = false;
             maskTextBoxValue = "";
+            status = IPStatus.TimedOut;
+            webBrowser1.ScriptErrorsSuppressed = true;
+
             saveFileDialog = new SaveFileDialog
             {
-
                 InitialDirectory = $@"C:\Users\{Environment.UserName}\Desktop\",
-                FileName = "Screenshot",
+                FileName = $"Screenshot №1",
                 CheckPathExists = true,
                 OverwritePrompt = true,
             };
-
-
-            webBrowser1.ScriptErrorsSuppressed = true;
         }
 
         async void TimerToSmoothlyRunForm_Tick(object sender, EventArgs e)
         {
             /*Плавное появление формы*/
-            if (counter == 0)
+            if (!counterTrueOrFalse)
             {
                 await Task.Delay(150);
-                counter++;
+                counterTrueOrFalse = true;
             }
 
             if (Opacity == 1)
@@ -57,35 +72,69 @@ namespace FindByIp
                 Opacity += .04;
         }
 
+        void MaskedTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (!string.IsNullOrWhiteSpace(maskedTextBox1.Text))
+                {
+                    e.SuppressKeyPress = true;
+                    Button1_Click(sender, e);
+                }
+            }
+
+            else
+                textBox1.Text = "";
+        }
+
         void Button1_Click(object sender, EventArgs e)
         {
+            MessageBox.Show($"{maskedTextBox1.Text.Length}");
+
             if (maskTextBoxValue != maskedTextBox1.Text)
             {
                 if (IPAdressExists(maskedTextBox1.Text))
                 {
                     maskTextBoxValue = maskedTextBox1.Text;
 
-                    if (panelForInformation.BackColor != SystemColors.GradientActiveCaption)
+                    try
                     {
-                        linkLabel1.Visible = false;
-                        panelForInformation.BackColor = SystemColors.GradientActiveCaption;
+                        Ping ping = new Ping();
+                        PingReply reply = ping.Send(@"ya.ru");
+                        status = reply.Status;
+
+                        if (panelForInformation.BackColor != SystemColors.GradientActiveCaption)
+                        {
+                            linkLabel1.Visible = false;
+                            panelForInformation.BackColor = SystemColors.GradientActiveCaption;
+                        }
+
+                        webBrowser1.Visible = true;
+
+                        using (WebClient wc = new WebClient())
+                        {
+                            Match match = Regex.Match(wc.DownloadString($"http://free.ipwhois.io/json/{maskedTextBox1.Text}"),
+                                "\"continent\":\"(.*?)\",(.*?)\"country\":\"(.*?)\",(.*?)\"region\":\"(.*?)\",\"city\":\"(.*?)\",\"latitude\":\"(.*?)\",\"longitude\":\"(.*?)\",(.*?)\"timezone_gmt\":\"(.*?)\"");
+
+                            webBrowser1.Url = new Uri($"https://www.google.com/maps/@?api=1&map_action=map&center={match.Groups[7].Value},{match.Groups[8].Value}", UriKind.Absolute);
+
+                            textBox1.Text = "Континент: " + match.Groups[1].Value + "\r\n" + "Страна: " + match.Groups[3].Value + "\r\n"
+                                + "Регион: " + match.Groups[5].Value + "\r\n" + "Город: " + match.Groups[6].Value + "\r\n"
+                                + "Широта: " + match.Groups[7].Value + "\r\n" + "Долгота: " + match.Groups[8].Value + "\r\n" + "Часовой пояс: " + match.Groups[10].Value;
+
+                            saveFileDialog.FileName = $"{match.Groups[6].Value}, {match.Groups[7].Value}, {match.Groups[8].Value}";
+                            timerForSlidingPanelInformation.Start();
+                        }
                     }
 
-                    webBrowser1.Visible = true;
-
-                    using (WebClient wc = new WebClient())
+                    catch (Exception)
                     {
-                        Match match = Regex.Match(wc.DownloadString($"http://free.ipwhois.io/json/{maskedTextBox1.Text}"),
-                            "\"continent\":\"(.*?)\",(.*?)\"country\":\"(.*?)\",(.*?)\"region\":\"(.*?)\",\"city\":\"(.*?)\",\"latitude\":\"(.*?)\",\"longitude\":\"(.*?)\",(.*?)\"timezone_gmt\":\"(.*?)\"");
+                        if (linkLabel1.Visible)
+                            linkLabel1.Visible = false;
 
-                        webBrowser1.Url = new Uri($"https://www.google.com/maps/@?api=1&map_action=map&center={match.Groups[7].Value},{match.Groups[8].Value}", UriKind.Absolute);
-
-                        textBox1.Text = "Континент: " + match.Groups[1].Value + "\r\n" + "Страна: " + match.Groups[3].Value + "\r\n"
-                            + "Регион: " + match.Groups[5].Value + "\r\n" + "Город: " + match.Groups[6].Value + "\r\n"
-                            + "Широта: " + match.Groups[7].Value + "\r\n" + "Долгота: " + match.Groups[8].Value + "\r\n" + "Часовой пояс: " + match.Groups[10].Value;
+                        label1.Visible = true;
+                        button2.Visible = true;
                     }
-
-                    timerForSlidingPanelInformation.Start();
                 }
 
                 else
@@ -100,13 +149,40 @@ namespace FindByIp
             else
             {
                 if (IPAdressExists(maskedTextBox1.Text))
-                    webBrowser1.Visible = true;
+                {
+                    try
+                    {
+                        Ping ping = new Ping();
+                        PingReply reply = ping.Send(@"ya.ru");
+                        status = reply.Status;
 
-                timerForSlidingPanelInformation.Start();
+                        webBrowser1.Visible = true;
+                        timerForSlidingPanelInformation.Start();
+                    }
+
+                    catch (Exception)
+                    {
+                        if (linkLabel1.Visible)
+                            linkLabel1.Visible = false;
+                        label1.Visible = true;
+                        button2.Visible = true;
+                    }
+                }
             }
         }
 
-        bool IPAdressExists(string ipForComparison) => IPAddress.TryParse(ipForComparison, out IPAddress address);
+        bool IPAdressExists(string ipForComparison) =>
+            IPAddress.TryParse(ipForComparison, out IPAddress address);
+
+        void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panelForInformation.BackColor = SystemColors.GradientActiveCaption;
+            maskedTextBox1.Clear();
+            linkLabel1.Visible = false;
+            textBox1.Clear();
+
+            Process.Start("https://ru.wikipedia.org/wiki/IPv4#%D0%9A%D0%BB%D0%B0%D1%81%D1%81%D0%BE%D0%B2%D0%B0%D1%8F_%D0%B0%D0%B4%D1%80%D0%B5%D1%81%D0%B0%D1%86%D0%B8%D1%8F");
+        }
 
         void TimerForSlidingPanelInformation_Tick(object sender, EventArgs e)
         {
@@ -145,11 +221,19 @@ namespace FindByIp
         {
             Bitmap screenshot = new Bitmap(Width - 16, Height - 9);
             Graphics graphics = Graphics.FromImage(screenshot);
-            /*обрезаем тень по краям формы*/
-            graphics.CopyFromScreen(Location.X + 8, Location.Y + 1, 0, 0, screenshot.Size);
+            graphics.CopyFromScreen(Location.X + 8, Location.Y + 1, 0, 0, screenshot.Size); /*обрезаем тень по краям формы*/
 
             saveFileDialog.Title = "Сохранение скриншота";
             saveFileDialog.Filter = "JPEG (*.jpg; *.jpeg; *.jpe) | *.jpg; *jpeg; *.jpe|PNG (*.png) | *.png|BMP (*.bmp) | *.bmp";
+
+            filesInFolder = Directory.GetFiles(saveFileDialog.InitialDirectory, "Screenshot №*");
+
+            if (filesInFolder.Any())
+            {
+                filesInFolder[filesInFolder.Length - 1] = filesInFolder[filesInFolder.Length - 1].Substring(filesInFolder[filesInFolder.Length - 1].IndexOf('№') + 1);
+                digitsInTheFileName = Convert.ToByte(filesInFolder[filesInFolder.Length - 1].Remove(filesInFolder[filesInFolder.Length - 1].IndexOf('.')));
+                saveFileDialog.FileName = $"Screenshot №{++digitsInTheFileName}";
+            }
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
@@ -166,31 +250,13 @@ namespace FindByIp
                 WindowState = FormWindowState.Normal;
         }
 
-        void CloseToolStripMenuItem_Click(object sender, EventArgs e) => Close();
+        void Button2_Click(object sender, EventArgs e) =>
+            Process.Start("control.exe", "netconnections");
 
-        void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            panelForInformation.BackColor = SystemColors.GradientActiveCaption;
-            maskedTextBox1.Clear();
-            linkLabel1.Visible = false;
-            textBox1.Clear();
-
-            Process.Start("https://ru.wikipedia.org/wiki/IPv4#%D0%9A%D0%BB%D0%B0%D1%81%D1%81%D0%BE%D0%B2%D0%B0%D1%8F_%D0%B0%D0%B4%D1%80%D0%B5%D1%81%D0%B0%D1%86%D0%B8%D1%8F");
-        }
-
-        void MaskedTextBox1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                if (!string.IsNullOrWhiteSpace(maskedTextBox1.Text))
-                {
-                    e.SuppressKeyPress = true;
-                    Button1_Click(sender, e);
-                }
-            }
-        }
+        void CloseToolStripMenuItem_Click(object sender, EventArgs e) =>
+            Close();
 
         void Form1_FormClosing(object sender, FormClosingEventArgs e) =>
-            Registry.CurrentUser.OpenSubKey(pathToRegistryFolder, true).DeleteValue("FindByIp.exe");
+            Registry.CurrentUser.OpenSubKey(PATH_TO_REGISTRY_FOLDER, true).DeleteValue("FindByIp.exe");
     }
 }
